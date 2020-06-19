d66d457d556b07f556c85b29db3d1ea3651212c2
Ignacio Vizzo <ignaciovizzo@gmail.com>
2020-06-19 15:59:33 -0300

    Add custom_target to run test-coverage by hand

--- cmake/TestCoverage.cmake
+++ cmake/TestCoverage.cmake
@@ -1,9 +1,14 @@
 # Code Coverage Configuration
 option(ENABLE_COVERAGE "Enable coverage reporting" OFF)
 if(ENABLE_COVERAGE)
-    # NOTE: Coverage only works/makes sense with debug builds
-    set(CMAKE_BUILD_TYPE "Debug")
-    set(CXX_COVERAGE_FLAGS "-fprofile-instr-generate -fcoverage-mapping")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CXX_COVERAGE_FLAGS}")
-    message(STATUS "Enabling coverage instrumentation: ${CXX_COVERAGE_FLAGS}")
+  # NOTE: Coverage only works/makes sense with debug builds
+  set(CMAKE_BUILD_TYPE "Debug")
+  set(CXX_COVERAGE_FLAGS "-fprofile-instr-generate -fcoverage-mapping")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CXX_COVERAGE_FLAGS}")
+  message(STATUS "Enabling coverage instrumentation: ${CXX_COVERAGE_FLAGS}")
+
+  add_custom_target(test-coverage
+                    COMMENT "Run test coverage for all sources expect for tests"
+                    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+                    COMMAND ${CMAKE_SOURCE_DIR}/.ci/test_coverage.sh)
 endif(ENABLE_COVERAGE)
